662130edef33d4f2a405a77fde87db1245c3fe0b
VIJAY KUMAR <vijjulu090304@gmail.com>
2025-09-20 14:36:10 +0530

    Update installation_steps.sh

---
+++ week1/Task0/installation_steps.sh
@@ -0,0 +1,31 @@
+# Task 0: Tool Installation and Verification
+
+## 📌 Objective
+To install and verify the required tools for the RISC-V SoC Tapeout program.
+
+---
+
+## 🛠 Tools Used
+### 1. Yosys
+- Purpose: Open-source framework for RTL synthesis.
+- Use: Converts Verilog HDL code into a gate-level netlist.
+
+### 2. Icarus Verilog (Iverilog)
+- Purpose: Open-source Verilog simulation tool.
+- Use: Used for compiling and simulating Verilog code.
+
+### 3. GTKWave
+- Purpose: Waveform viewer.
+- Use: Visualizes output waveforms from simulation.
+
+---
+
+## ✅ Verification Screenshots
+- Yosys installation check
+  ![Yosys Installed](Images/yosys_done.png)
+
+- Iverilog installation check
+  ![Iverilog Installed](Images/iverilog_done.png)
+
+- GTKWave installation check
+  ![GTKWave Installed](Images/gtkwave_done.png)
